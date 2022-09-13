e391ceac55032c6c4bae263a33b56326c066ae68
Jude Joubert <joubertjude@gmail.com>
2022-09-13 11:21:03 -0500

    FUCK

--- bot.py
+++ bot.py
@@ -1,135 +1,66 @@
-from datetime import date, datetime
+from datetime import *
 from tradingtools import *
 from threading import *
-import time
 
 class tradingbot(Thread):
 
     ticker = ''
     shouldRun = True
-    initialBuy = 75000 #How much of an asset to buy in USD (only bought once the program is run and if there is not a position in this asset)
-    terms = {}
-
-    """
-
-    ticker = the ticker of the coin
-    shouldRun = controls when the bot should terminate
-    initialBuy = how much of the coin the bot should buy on initialize if a position isnt already held
-    terms = conditions on which the bot should buy and sell
-        prices & time = takes note of the price at a certain time
-                        (used to get delta T and determine percentage change in amount of time)
-                        the list positions correlate to each other
-        
-        buyPrices = prices the bot potentially should buy at
-        sellPrices = prices the bot should potentially sell at
-                     arranged from worst case sceneraio to best case scenerio
-                     it sells 5% of the current position for every milestone hit
-                     until the current position doesnt hold enough to sell anymore (user defined)
-        
-    percentUpSell = by how much the price of the coin should rise before it the bot should sell by one milestone
-    percentDownBuy = by how much the price of the coin should dip before the bot should buy in again
-
-    """
+    initBuy = 3
+    position = None
+    start_time = None
+    last_action_time_min = None
+    sellWhenPercentIsOver = 0.05
+    verbose = False 
 
     def __init__(self, ticker) -> None:
-        Thread.__init__(self)
+        Thread.__init__(self);
         self.ticker = ticker
+        self.position = 0
 
-        self.terms.update(
-            {
-                "prices" : [],
-                "time" : [],
-                "lastAction" : "",  
-                "buyPrices" : [],
-                "sellPrices" : [],
-                "percentChange" : 0.05
-            }
-        )
-
-    """
-    Get current price of the coin
+        self.start_time = datetime.now()
+        
     """
 
-    def currentPrice(self) -> float:
-        return ttCurrentPrice(self.ticker)
+    Tools
 
-    """"
-    Buy function
     """
 
-    def position(self) -> float:
-        return ttgetPostion(self.ticker)
-
-    def buy(self, amountInUSD: float):
-        amount = amountInUSD / self.currentPrice()
+    def buy(self, amount: float):
         ttbuy(self.ticker, amount)
 
-    def sell(self, amountInUSD: float):
-        amount = amountInUSD / self.currentPrice()
-
-    def autoBuy(self):
-        ttbuy(self.ticker, ttgetPostion(ticker = self.ticker)/3)
-
-    def autoSell(self):
-        ttsell(self.ticker, ttgetPostion()/3)
+    def sell(self, amount: float):
+        ttsell(self.ticker, amount)
 
     """
-    Get difference between two times
-    """
 
-    def getDeltaT(self, date1, date2):
-        return date1 - date2
-
-    def shouldSell(self) -> bool:
-        for i in range(len(self.terms.get("prices"))):
-            percentChange = 1 + self.terms.get("percentChange")
-            
-            if self.terms.get("prices")[i] * percentChange <= self.terms.get("prices")[-1]:
-               return True
-        
-        return False
-
-    def shouldBuy(self) -> bool:
-        for i in range(len(self.terms.get("prices"))):
-            percentChange = 1 - self.terms.get("percentChange")
-            
-            if self.terms.get("prices")[i] * percentChange >= self.terms.get("prices")[-1]:
-               return True
-        
-        return False
+    Trading functions
 
     """
-    Get the amount of time it took to accomplish your selling point
-    The amount of time it took to increase <desired percent> at which the bot sold at
-    """
-    def getTimeTakenForMilestone(self):
-        for i in range(len(self.terms.get("prices"))):
-            if self.shouldSell():
-                return i * 5 #Goal met
 
-        return -1 #Goal not met
-
-    """
-    Update terms dictonary
-    """
-    def updatePrices(self):
-        pass
-
-    #Put algorithmic stuff here
     def update(self):
-        if(self.shouldBuy):
-            self.autoBuy()
-            print("Buy at ", datetime.now())
-        
-        elif(self.shouldSell): 
-            self.autoSell()
-            print("Sell at ", datetime.now())
 
-        else:
-            print("Nothing at ", datetime.now())
-        
+        self.buy(1)
+        self.sell(1)
 
-    #On terminate
+        #Sell if our coin has increased by 3% or more from our buying point
+        #if self.position >= 0 and :
+
+        #Buy if in the past couple of hours our coin has decreased by at least 3% from our selling point
+        #if self.start_time < datetime.now() &&
+
+        #Debug stuff
+        if self.verbose:
+            tempDict = {
+                'ticker' : self.ticker,
+                'shouldRun' : self.shouldRun,
+                'initBuy' : self.initBuy,
+                'position' : self.position,
+                'start_time' : self.start_time,
+                'sellWhenPercentIsOver' : self.sellWhenPercentIsOver,
+            }
+            print(tempDict)
+
     def terminate(self):
         pass
 
--- tradingtools.py
+++ tradingtools.py
@@ -2,26 +2,23 @@ import alpaca_trade_api as tradeapi
 
 #Base info for trading only paper right now
 BASE_URL = "https://paper-api.alpaca.markets"
-KEY_ID = "PKNQ638MQIWY658JF8UY"
-SECRET_KEY = "xLlFt1iGRy9f0wY1wp9V7O4gVTP2Z1wtslTiKELg"
+KEY_ID = "PKMF9F5R660ATDJD6OFL"
+SECRET_KEY = "POWp5pKUxWC5keOikJXHOzyjwafYxQkXCFpdYLWZ"
 api = tradeapi.REST(key_id=KEY_ID,secret_key=SECRET_KEY,base_url="https://paper-api.alpaca.markets")
+#api = tradeapi.REST(key_id=KEY_ID,secret_key=SECRET_KEY,base_url="https://paper-api.alpaca.markets", api_version = 'v2')
 buytype = "ioc"
-current_position = 0
 
 def ttbuy(ticker, amount) -> None:
     api.submit_order(ticker, qty = amount, side = 'buy', time_in_force = buytype)
-    current_position += amount
 
 def ttsell(ticker, amount) -> None:
     api.submit_order(ticker, qty = amount, side = 'sell', time_in_force = buytype)
-    current_position -= amount
 
-def ttgetPostion(ticker) -> float:
-    return current_position
-    #return float(api.get_position(ticker).qty) API ERROR smh
+def ttgetPosition(ticker) -> float:
+    return 
 
 def ttGetPrice(ticker) -> float:
     pass
 
-def ttCurrentPrice(ticker):
-    return float(api.get_position(ticker).current_price)
+def ttCurrentPrice(self, ticker):
+    return 
